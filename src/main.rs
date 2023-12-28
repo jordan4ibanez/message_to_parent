529021cfaa5253f9e9b7c86c38396b3f10f3d09c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-28 07:50:50 -0500

    Add an extremely nice implementation

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,44 @@ use std::{
 
 ////////////////////////////////////////////////////////! End Imports
 
-struct Message {
-  pub closure: Rc<dyn Fn(&mut Parent)>,
+struct MessageToParent {
+  side_effects: Vec<fn(&mut Parent)>,
 }
 
-impl Drop for Message {
+impl MessageToParent {
+  pub fn new() -> Self {
+    MessageToParent {
+      side_effects: vec![],
+    }
+  }
+
+  pub fn add_side_effect(&mut self, new_side_effect: fn(&mut Parent)) {
+    self.side_effects.push(new_side_effect);
+  }
+
+  pub fn run_side_effects(&self, parent: &mut Parent) {
+    for side_effect in &self.side_effects {
+      side_effect(parent);
+    }
+  }
+
+  ///
+  /// Clears out all currently pushed side effects for the Message.
+  /// 
+  /// **Don't use this unless you know what you're doing!**
+  ///
+  pub fn clear_side_effects(&mut self) {
+    self.side_effects.clear();
+  }
+}
+
+impl Drop for MessageToParent {
   fn drop(&mut self) {
     println!("Message was dropped!")
   }
 }
 
-////////////////////////////////////////////////////////! End Message
+////////////////////////////////////////////////////////! End MessageToParent
 
 struct Parent {
   child: Option<Rc<RefCell<Child>>>,
@@ -43,9 +70,10 @@ impl Parent {
 
   pub fn main(&mut self) {
     println!("parent: running mutation on child");
+
     let mutation_attempt = self.child.as_deref().unwrap().borrow_mut().mutate_parent();
 
-    (mutation_attempt.unwrap().closure)(self);
+    mutation_attempt.unwrap().run_side_effects(self);
   }
 
   pub fn parent_mutate_procedure(&mut self) {
@@ -70,13 +98,17 @@ impl Child {
     Rc::new(RefCell::new(Self { parent: None }))
   }
 
-  pub fn mutate_parent(&mut self) -> Option<Message> {
+  pub fn mutate_parent(&mut self) -> Option<MessageToParent> {
     println!("child: Attempting to mutate parent!");
-    Some(Message {
-      closure: Rc::new(|parent| {
-        parent.parent_mutate_procedure();
-      }),
-    })
+    let mut returning_message = MessageToParent::new();
+
+    returning_message.add_side_effect(|_| println!("Parental advisory"));
+
+    returning_message.add_side_effect(|parent| {
+      parent.parent_mutate_procedure();
+    });
+
+    Some(returning_message)
   }
 }
 
